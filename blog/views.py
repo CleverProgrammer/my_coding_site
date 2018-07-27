7ab273a051d1b7d6d9f286f71ed7b3b512e7adef
Rafeh Qazi <qazi@cleverprogrammer.com>
2018-07-27 03:17:05 -0700

    Add dynamic posts.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts)
+    return render(request, 'blog/post_list.html', {'posts': posts})
